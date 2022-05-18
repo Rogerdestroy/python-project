698843e78d07719738f9a32a6e331436a0655e6e
Rogerdestroy <roge000000mymail@gmail.com>
2022-05-18 20:42:22 +0800

    pygame01

---
+++ pygame/sprite.py
@@ -0,0 +1,35 @@
+import pygame
+
+FPS = 60
+WHITE = (255,255,255)
+WIDTH = 500
+HIGHT = 600
+
+pygame.init() #遊戲初始化
+screen = pygame.display.set_mode((WIDTH,HIGHT)) #創建視窗
+pygame.display.set_caption("main") #命名視窗
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+    self.image
+
+
+running = True
+
+while running:
+    clock.tick(FPS) #一秒鐘之內執行次數 FPS
+    
+    #取得輸入
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    #畫面顯示
+    screen.fill(WHITE)  #視窗填滿
+    pygame.display.update() #更新   
+    
+    
+pygame.quit()
+            
+        
